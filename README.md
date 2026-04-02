a81c6c27b09a739cbb6dcb33ae9b5618367db0fb
Kubin Samuvel <kubinsamuvel@gmail.com>
2026-04-02 17:23:34 +0530

    update code

--- README.md
+++ README.md
@@ -78,7 +78,7 @@ Marchfast/
 ---
 
 ## API Base URL
-`http://  https://marchfastn.shop/api/api/`
+`http://  https://api.marchfastn.shop/api/api/`
 
 ---
 
--- files/axiosClient.js
+++ files/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || ' https://marchfastn.shop/api'
+const API_BASE_URL = import.meta.env.VITE_API_URL || ' https://api.marchfastn.shop/api'
 
 const axiosClient = axios.create({
   baseURL: API_BASE_URL,
